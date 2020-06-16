b8ea8ab76ebf0593f11716063c40ccaba457051e
X8D <romanov.and@gmail.com>
2020-06-16 21:07:36 +0300

    some fix

--- src/test/java/helpers/Environment.java
+++ src/test/java/helpers/Environment.java
@@ -6,6 +6,8 @@ public class Environment {
 
     public final static String
             selenoidUrl = System.getProperty("selenoid_url", "selenoid.autotests.cloud"),
+            selenoidUsername = System.getProperty("selenoid_username", ""),
+            selenoidPassword = System.getProperty("selenoid_password", ""),
             mailruUrl = System.getProperty("mailruUrl", "https://mail.ru");
 
     public final static boolean
